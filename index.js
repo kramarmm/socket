8d9db438fd7c628a04d001264582cc33f20962f4
kramarmm <mariakr@clioonline.dk>
2018-11-16 10:04:41 +0200

    fix

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const server = require('http').createServer();
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server });
 
-const clients = {};
-
 function heartbeat() {
   this.isAlive = true;
 }
@@ -28,37 +26,17 @@ wss.on('connection', ws => {
     try {
       const msg = JSON.parse(message);
 
-      switch (msg.type) {
-        case 'add-client':
-          clients[msg.data.username] = {
-            ws: ws.id,
-            ...msg.data,
-          };
-          break;
-
-        case 'send-message':
-          wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify(msg.data));
-            }
-          });
-          break;
-
-        default:
-          console.log('Unknown type');
-      }
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(msg.data));
+        }
+      });
     } catch (error) {
       console.log(`Error on handling message: ${error}`);
     }
   });
 
   ws.on('close', (code, reason) => { // CLOSE
-  	for(let name in clients) {
-  		if(clients[name].ws === ws.id) {
-  			delete clients[name];
-  			break;
-  		}
-  	}
     console.log(`${code} - reason: ${reason}`);
     clearInterval(interval);
   });
